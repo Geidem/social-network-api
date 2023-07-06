7c1b844e9bfe72abf338ce823bdb84c89122776d
Unbroken <gibby.eidem@gmail.com>
2023-07-06 15:39:26 -0600

    updated models

--- models/Thought.js
+++ models/Thought.js
@@ -1,31 +1,6 @@
 const {Schema, model} = require('mongoose');
+const reactionSchema = require('./Reaction');
 
-const reactionSchema = new Schema ({
-    reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId()
-    },
-    reactionBody: {
-        type: String,
-        required: true,
-        maxlength: 280
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => formatDate(createdAtVal)
-    }
-},
-{
-    toJSON: {
-        getters: true
-    },
-    id: false
-});
 
 const thoughtSchema = new Schema ({
     thoughtText: {
